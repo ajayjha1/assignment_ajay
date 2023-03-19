0d6a5e26ae0a30469feae51fabf5dee013c23538
ajayjha1 <ajayjha1886@gmail.com>
2023-03-19 19:59:37 +0530

    some changes

---
+++ src/actions/action.js
@@ -0,0 +1,31 @@
+export const updateFormField = () => {
+    return {
+      type: "UPDATE_FORM_FIELD",
+
+    };
+  };
+  
+  export const updateFormCategory = () => {
+    return {
+      type: "UPDATE_FORM_CATEGORY",
+    };
+  };
+  
+  export const clearForm = () => {
+    return {
+      type: "CLEAR_FORM",
+    };
+  };
+  
+  export const addCategory = () => {
+    return {
+      type: "ADD_CATEGORY",
+    };
+  };
+  
+  export const updateCategory = () => {
+    return {
+      type: "UPDATE_CATEGORY",
+    };
+  };
+  
